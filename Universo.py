26323f937c7b6bbd6620a72d96b75e385f33924f
Javier Miles <javier.miles@inacapmail.cl>
2023-09-25 16:45:32 -0300

    Agregar elementos uni y bidimensionales

--- Universo.py
+++ Universo.py
@@ -1,2 +1,18 @@
-class Unidimensional: 
-    pass
+from abc import ABC, abstractclassmethod
+class Unidimensional(ABC): # Aplico Abstracción
+    def __init__(self):
+        self.volumen=0
+        self.masa=0
+        print("Soy Unidimensional")
+
+class Bidimensional(Unidimensional): # Aplicar Herencia
+    def __init__(self): 
+        super().__init__() # Llamado de constructor del padre
+        print("Hola, soy un elemento bidimensional")
+
+
+
+if(__name__ == "__main__"):
+    print("Estoy en el programa principal")
+    c = Bidimensional()
+    print(c.volumen)
